e3cb808be5f92ed3c471b4017054253268c1654b
William Eddy <william-eddy@outlook.com>
2021-02-20 15:32:17 +0000

    Date now being displayed

--- server.py
+++ server.py
@@ -12,9 +12,9 @@ def getDatabaseConnection():
     )
     return dbConnection
 
-def executeQuery(query, values=[]):
+def executeQuery(query, values=()):
     connection = getDatabaseConnection()
-    cursor = connection.cursor()
+    cursor = connection.cursor(prepared=True)
     cursor.execute(query,values)
     data = cursor.fetchall()
     connection.close()
@@ -29,8 +29,9 @@ def upcomingEvents():
 @app.route("/upcomingEvents/viewDates/<eventID>")
 def viewDates(eventID):
     if request.method =='GET':
-        performanceData = executeQuery("SELECT p.performanceID, v.venueCity, v.venueName, DATE_FORMAT(p.performanceDateTime, "%D %M %Y")AS date FROM performances p INNER JOIN venues v ON p.venueID = v.venueID WHERE p.eventID = ?",[eventID])
-        return render_template("viewDates.html", date=performanceData)
+        eventID = int(eventID)
+        performanceData = executeQuery("SELECT p.performanceID, v.venueCity, v.venueName, DATE_FORMAT(p.performanceDateTime, '%D %M %Y')AS date FROM performances p INNER JOIN venues v ON p.venueID = v.venueID WHERE p.eventID = %s",(eventID,))
+        return render_template("viewDates.html", performances=performanceData)
 
 
 if __name__ == "__main__":
