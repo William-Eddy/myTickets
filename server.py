6156ca2f03cb5e0028b8a381de915cb2ad4855b0
William Eddy <william-eddy@outlook.com>
2021-02-20 10:25:10 +0000

    Readme updated with server running instructions

--- server.py
+++ server.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 
 if __name__ == "__main__":
     app.run(debug=True, port=80, host='0.0.0.0')
+
+#To run on network:
+#export FLASK_APP=server.py
+#. venv/bin/activate
+#flask run --host 0.0.0.0
