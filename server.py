6a5de3442149122bac34abfa1a1fc5359bc9bd96
William Eddy <william-eddy@outlook.com>
2021-02-20 13:23:49 +0000

    Database connection working

--- server.py
+++ server.py
@@ -1,14 +1,24 @@
-from flask import Flask
-from flask_mysqldb import MySQL
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Kettering1'
-app.config['MYSQL_DB'] = 'myTickets'
-
-mysql = MySQL(app)
+from flask import Flask, redirect, request,render_template, jsonify, send_from_directory
+import mysql.connector
 
 app = Flask(__name__)
+
+def getDatabaseConnection():
+    dbConnection = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password="Kettering1",
+      database="myTickets"
+    )
+    return dbConnection
+
+def executeQuery(query, values=[]):
+    connection = getDatabaseConnection()
+    cursor = connection.cursor()
+    cursor.execute(query,values)
+    data = cursor.fetchall()
+    connection.close()
+    return data
 
 if __name__ == "__main__":
     app.run(debug=True, port=80, host='0.0.0.0')
